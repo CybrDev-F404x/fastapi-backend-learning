0a60bc903b863ac018d7e3547660e469fc5fa445
CybrDev-F404x <frankorisro@gmail.com>
2026-01-30 00:50:01 -0400

    feat: secure database connection with env vars

--- db/clients/client.py
+++ db/clients/client.py
@@ -1,7 +1,11 @@
+import os
+from dotenv import load_dotenv
 from pymongo import MongoClient
+
+load_dotenv()
 
 # Base de datos local
 # db_client = MongoClient().local.users
 
 # Base de datos remota
-db_client = MongoClient("mongodb+srv://test:del1al9@clusterfastapi.v1zluv0.mongodb.net/?appName=ClusterFastAPI").test.users
+db_client = MongoClient(os.environ.get("MONGODB_URI")).test.users
